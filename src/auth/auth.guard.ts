7e2bca303e31a69547f2de8587849cfeb34c6cfd
AnushervonKhidirov <ankhid98@gmail.com>
2026-02-04 16:21:44 +0500

    auth guard added

---
+++ src/auth/auth.guard.ts
@@ -0,0 +1,32 @@
+import type { Request } from 'express';
+
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+
+import { TokenService } from 'src/token/token.service';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private readonly tokenService: TokenService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest<Request>();
+    const token = this.extractTokenFromHeader(request);
+    if (!token) throw new UnauthorizedException();
+
+    const [userPayload, err] = await this.tokenService.verifyAccessToken(token);
+    if (err) throw err;
+
+    request['user'] = userPayload;
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request) {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
