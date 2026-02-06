1b7158486796f1252b9ad303c592b9dbfda6e888
AnushervonKhidirov <ankhid98@gmail.com>
2026-02-06 17:30:41 +0500

    cookie parser added

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import './utils/config/dayjs.config';
 import { NestFactory, HttpAdapterHost } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import cookieParser from 'cookie-parser';
 import { HttpExceptionFilter } from './filter/exception.filter';
 import { AppModule } from './app.module';
 
@@ -15,6 +16,7 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
 
   app.useGlobalFilters(new HttpExceptionFilter(httpAdapter));
+  app.use(cookieParser());
   SwaggerModule.setup('api', app, documentFactory);
   await app.listen(process.env.PORT ?? 3000);
 }
