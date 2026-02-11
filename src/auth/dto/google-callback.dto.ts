77a9c4ed7c75c1c4153efaeb6f6182312a5a7d9c
AnushervonKhidirov <ankhid98@gmail.com>
2026-02-11 08:23:55 +0500

    google oauth example added

--- src/auth/dto/google-callback.dto.ts
+++ src/auth/dto/google-callback.dto.ts
@@ -1,6 +1,9 @@
+import { ApiProperty, ApiSchema } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
+@ApiSchema({ name: 'Google Callback DTO' })
 export class GoogleCallbackDto {
+  @ApiProperty({ example: 'somecodeprovidedbygoogleoauth' })
   @IsString()
   code: string;
 }
