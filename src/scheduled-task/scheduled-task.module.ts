fbc21b8bc7ac4ff3d3b4d55ce5df924dd3ed3423
AnushervonKhidirov <ankhid98@gmail.com>
2026-03-08 15:12:24 +0500

    remove exprired codes

--- src/scheduled-task/scheduled-task.module.ts
+++ src/scheduled-task/scheduled-task.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TokenModule } from 'src/token/token.module';
+import { VerificationCodeModule } from 'src/verification-code/verification-code.module';
 import { ScheduledTaskService } from './scheduled-task.service';
 
 @Module({
-  imports: [TokenModule],
+  imports: [TokenModule, VerificationCodeModule],
   providers: [ScheduledTaskService],
 })
 export class ScheduledTaskModule {}
--- src/scheduled-task/scheduled-task.service.ts
+++ src/scheduled-task/scheduled-task.service.ts
@@ -1,13 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { TokenService } from 'src/token/token.service';
+import { VerificationCodeService } from 'src/verification-code/verification-code.service';
 
 @Injectable()
 export class ScheduledTaskService {
-  constructor(private readonly tokenService: TokenService) {}
+  constructor(
+    private readonly tokenService: TokenService,
+    private readonly verificationCodeService: VerificationCodeService,
+  ) {}
 
   @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
   async deleteExpiredTokens() {
-    await this.tokenService.deleteExpiredTokens();
+    await this.tokenService.deleteExpired();
+  }
+
+  @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
+  async deleteExpiredCodes() {
+    await this.verificationCodeService.deleteExpired();
   }
 }
