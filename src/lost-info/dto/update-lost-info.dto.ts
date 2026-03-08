988f1b642e3d26b33ea452ec133ba8b355e08883
AnushervonKhidirov <ankhid98@gmail.com>
2026-03-08 21:55:14 +0500

    lost info connroller updated

--- src/lost-info/dto/update-lost-info.dto.ts
+++
@@ -1,5 +0,0 @@
-import { ApiSchema, PartialType } from '@nestjs/swagger';
-import { CreateLostInfoDto } from './create-lost-info.dto';
-
-@ApiSchema({ name: 'Update Lost Info DTO' })
-export class UpdateLostInfoDto extends PartialType(CreateLostInfoDto) {}
--- src/lost-info/lost-info.controller.ts
+++ src/lost-info/lost-info.controller.ts
@@ -4,7 +4,6 @@ import type { TokenDecoded } from 'src/token/token.type';
 import {
   Controller,
   Post,
-  Patch,
   Delete,
   Param,
   Body,
@@ -19,7 +18,6 @@ import { AuthGuard } from 'src/auth/guard/auth.guard';
 import { LostInfoService } from './lost-info.service';
 
 import { CreateLostInfoDto } from './dto/create-lost-info.dto';
-import { UpdateLostInfoDto } from './dto/update-lost-info.dto';
 
 @Controller('lost-info')
 export class LostInfoController {
@@ -27,7 +25,7 @@ export class LostInfoController {
 
   @UseGuards(AuthGuard)
   @Post(':petId')
-  async create(
+  async set(
     @Req() req: Request,
     @Param('petId', ParseIntPipe) petId: number,
     @Body(new ValidationPipe({ transform: true, whitelist: true }))
@@ -39,26 +37,6 @@ export class LostInfoController {
     const [lostInfo, err] = await this.lostInfoService.upsert({
       where: { petId, pet: { userId: tokenDecoded.sub } },
       data: { ...data, pet: { connect: { id: petId } } },
-    });
-
-    if (err) throw err;
-    return lostInfo;
-  }
-
-  @UseGuards(AuthGuard)
-  @Patch(':petId')
-  async update(
-    @Req() req: Request,
-    @Param('petId', ParseIntPipe) petId: number,
-    @Body(new ValidationPipe({ transform: true, whitelist: true }))
-    data: UpdateLostInfoDto,
-  ) {
-    const tokenDecoded = req['user'] as TokenDecoded | undefined;
-    if (!tokenDecoded) throw new UnauthorizedException();
-
-    const [lostInfo, err] = await this.lostInfoService.update({
-      where: { petId },
-      data,
     });
 
     if (err) throw err;
