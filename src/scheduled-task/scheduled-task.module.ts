b0dd35b138e1ab0eca01835917444da45c92b9ab
AnushervonKhidirov <ankhid98@gmail.com>
2026-03-09 20:25:45 +0500

    remove expired password page ids

--- src/scheduled-task/scheduled-task.module.ts
+++ src/scheduled-task/scheduled-task.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TokenModule } from 'src/token/token.module';
 import { VerificationCodeModule } from 'src/verification-code/verification-code.module';
+import { ResetPasswordModule } from 'src/reset-password/reset-password.module';
 import { ScheduledTaskService } from './scheduled-task.service';
 
 @Module({
-  imports: [TokenModule, VerificationCodeModule],
+  imports: [TokenModule, VerificationCodeModule, ResetPasswordModule],
   providers: [ScheduledTaskService],
 })
 export class ScheduledTaskModule {}
--- src/scheduled-task/scheduled-task.service.ts
+++ src/scheduled-task/scheduled-task.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
+
 import { TokenService } from 'src/token/token.service';
 import { VerificationCodeService } from 'src/verification-code/verification-code.service';
+import { ResetPasswordService } from 'src/reset-password/reset-password.service';
 
 @Injectable()
 export class ScheduledTaskService {
   constructor(
     private readonly tokenService: TokenService,
     private readonly verificationCodeService: VerificationCodeService,
+    private readonly resetPasswordService: ResetPasswordService,
   ) {}
 
   @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
@@ -18,5 +21,10 @@ export class ScheduledTaskService {
   @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
   async deleteExpiredCodes() {
     await this.verificationCodeService.deleteExpired();
+  }
+
+  @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
+  async deleteResetPasswordId() {
+    await this.resetPasswordService.deleteExpired();
   }
 }
