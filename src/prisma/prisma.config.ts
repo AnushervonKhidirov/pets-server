1ba491d7cb73b824ee5fb90cc9f3fc97005e4c02
AnushervonKhidirov <ankhid98@gmail.com>
2026-03-11 10:33:08 +0500

    prisma config updated

--- src/prisma/prisma.config.ts
+++ src/prisma/prisma.config.ts
@@ -1,8 +1,22 @@
-import { registerAs } from '@nestjs/config';
+import { PoolConfig } from 'mariadb';
 
-export default registerAs('database', () => ({
+import { registerAs } from '@nestjs/config';
+import { readFileSync } from 'node:fs';
+import { join } from 'node:path';
+
+function parseIntEnv(envString: string | undefined): number | undefined {
+  return envString ? Number.parseInt(envString) : undefined;
+}
+
+export default registerAs<PoolConfig>('database', () => ({
+  port: parseIntEnv(process.env.DB_PORT),
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  connectTimeout: parseIntEnv(process.env.DB_CONNECTION_TIMEOUT),
+  ssl: {
+    ca: readFileSync(join(process.cwd(), 'secret', 'ca.pem')).toString(),
+    rejectUnauthorized: true,
+  },
 }));
