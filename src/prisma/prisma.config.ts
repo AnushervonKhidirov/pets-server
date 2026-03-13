b0f50b23aebe47736b3f72929d8b4b27f58c0612
AnushervonKhidirov <ankhid98@gmail.com>
2026-03-13 22:07:32 +0500

    remove ssl sertificate

--- src/prisma/prisma.config.ts
+++ src/prisma/prisma.config.ts
@@ -1,30 +1,16 @@
 import { PoolConfig } from 'mariadb';
-
 import { registerAs } from '@nestjs/config';
-import { readFileSync, existsSync } from 'node:fs';
-import { join } from 'node:path';
 
 function parseIntEnv(envString: string | undefined): number | undefined {
   return envString ? Number.parseInt(envString) : undefined;
 }
 
 export default registerAs<PoolConfig>('database', () => {
-  const sslCertPath = join(process.cwd(), 'secret', 'ca.pem');
-  const useSsl =
-    process.env.DB_SSL !== 'false' && existsSync(sslCertPath);
-
   return {
     port: parseIntEnv(process.env.DB_PORT),
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    connectTimeout: parseIntEnv(process.env.DB_CONNECTION_TIMEOUT),
-    ...(useSsl && {
-      ssl: {
-        ca: readFileSync(sslCertPath).toString(),
-        rejectUnauthorized: true,
-      },
-    }),
   };
 });
