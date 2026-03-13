e0135e044ba83cb2cb64e1f9fa40a92af0ee56f8
AnushervonKhidirov <ankhid98@gmail.com>
2026-03-13 22:15:57 +0500

    compose updated

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { HttpExceptionFilter } from './filter/exception.filter';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: corsConfig });
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   const { httpAdapter } = app.get(HttpAdapterHost);
 
