c8efb39821fd38d07baad10b7b122eac66145e81
AnushervonKhidirov <ankhid98@gmail.com>
2026-03-15 17:34:36 +0500

    fixed

--- src/utils/config/ssl.config.ts
+++ src/utils/config/ssl.config.ts
@@ -4,8 +4,8 @@ import { readFileSync } from 'node:fs';
 import { join } from 'node:path';
 
 const httpsOptions: HttpsOptions = {
-  key: readFileSync(join(process.cwd(), '.secrets', 'private-key.pem')),
-  cert: readFileSync(join(process.cwd(), '.secrets', 'public-certificate.pem')),
+  key: readFileSync(join(process.cwd(), 'secret', 'private-key.pem')),
+  cert: readFileSync(join(process.cwd(), 'secret', 'public-certificate.pem')),
 };
 
 export default httpsOptions;
