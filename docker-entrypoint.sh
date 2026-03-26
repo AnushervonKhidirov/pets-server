857f1657185f7b876558560a0032de4bddaec767
AnushervonKhidirov <ankhid98@gmail.com>
2026-03-26 10:48:23 +0500

    entrypoints prisma command changed

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Syncing database schema..."
-npx prisma db push --config ./prisma.config.ts
+npx prisma migrate deploy --config ./prisma.config.ts
 
 echo "Starting application..."
 exec node dist/src/main
